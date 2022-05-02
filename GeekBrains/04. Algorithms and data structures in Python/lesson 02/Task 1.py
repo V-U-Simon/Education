19d234f3708020df8b5767f1229223aa6a4454c7
Willy Wonka <V-U-Simon@yandex.ru>
2022-05-02 22:08:07 +0300

    HW lesson 02
    Add two task

---
+++ GeekBrains/04. Algorithms and data structures in Python/lesson 02/Task 1.py
@@ -0,0 +1,55 @@
+# Task 1
+# Написать программу, которая будет складывать, вычитать, умножать или делить два числа
+# Числа и знак операции вводятся пользователем
+# После выполнения вычисления программа не должна завершаться, а должна запрашивать новые данные для вычислений
+# Завершение программы должно выполняться при вводе символа '0' в качестве знака операции
+# Если пользователь вводит неверный знак (не '0', '+', '-', '*', '/'),
+# то программа должна сообщать ему об ошибке и снова запрашивать знак операции
+# Также сообщать пользователю о невозможности деления на ноль, если он ввел 0 в качестве делителя
+# 
+
+# def calculator(sign=input('Введите операцию (+, -, *, / или 0 для выхода): '),
+#                digit_1=int(input('Введите первое число: ')),
+#                digit_2=int(input('Введите второе число: '))):
+
+
+def calculator():
+    """ Обычный калькулятор """
+
+    ACTIONS = {
+        "+": lambda digit_1, digit_2: digit_1 + digit_2,
+        "-": lambda digit_1, digit_2: digit_1 - digit_2,
+        "*": lambda digit_1, digit_2: digit_1 * digit_2,
+        "/": lambda digit_1, digit_2: digit_1 / digit_2,
+        "0": None
+    }
+
+    # VALIDATION
+    def check_sigh(sign):
+        try:
+            ACTIONS[sign]
+        except KeyError:
+            sign = check_sigh(input("Допустимые значения: '+', '-', '*', '/'б  '0' для вывода: "))
+        return sign
+
+    def check_digit(digit):
+        try:
+            digit = int(digit)
+        except ValueError:
+            digit = check_digit(input('Повторите ввод, т.к. вы ввели строку вместо числа: '))
+        return digit
+
+    sign = check_sigh(input('Введите операцию (+, -, *, / или 0 для вывода): '))
+    if sign == "0": return  # BASE CASE
+    digit_1 = check_digit(input('Введите число 1: '))
+    digit_2 = check_digit(input('Введите число 2: '))
+
+    # MAIN
+    print(ACTIONS[sign](digit_1, digit_2))
+
+    # RECURSION FUNCTION
+    calculator()
+
+
+if __name__ == "__main__":
+    calculator()
---
+++ GeekBrains/04. Algorithms and data structures in Python/lesson 02/Task 3.py
@@ -0,0 +1,31 @@
+# Task 3
+# Сформировать из введенного числа обратное по порядку входящих в него цифр и вывести на экран
+# Например, если введено число 3486, то надо вывести число 6843
+
+
+def decorator(func):
+    """ Декоратор для приведения результата цункции к int """
+
+    def inside_function(*args, **kwargs):
+        res = func(*args, **kwargs)
+        return int(res)
+
+    return inside_function
+
+
+@decorator
+def opposite_direction_numbers(number: int) -> str:
+    """ Возвращает в числа в обратном порядке """
+
+    front, tail = number // 10, number % 10
+    # if front == 0:
+    #     front = '0'
+
+    return tail if not front else str(tail) + str(opposite_direction_numbers(front))
+    #           ^ BASE CASE                       ^ RECURSIVE FUNCTION
+
+
+if __name__ == '__main__':
+    assert opposite_direction_numbers(123) == 321
+    assert opposite_direction_numbers(1230) == 321
+    assert opposite_direction_numbers(0) == 0
